d50499f1b54a82a528db3bf05a1514d845f43c28
changbin93 <626498551@qq.com>
2022-02-28 14:11:03 +0800

    create basic crud using renren-generator

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,35 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(1L);
+////        brandEntity.setName("华为");
+//        brandEntity.setDescript("华为");
+////        brandService.save(brandEntity);
+//        brandService.updateById(brandEntity);
+//        System.out.println("修改成功");
+
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        for (BrandEntity item:list) {
+            System.out.println(item);
+        }
+
     }
 
 }
